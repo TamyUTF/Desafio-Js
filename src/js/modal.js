13c1c228c34cad0a7e4b02c0c8c6275a6dc8835d
Tamyres <tamy.yu@hotmail.com>
2019-02-26 18:58:57 -0300

    fix modal contact

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,13 +1,18 @@
+import { isFavorite } from "./contact";
+
 const modalDiv = document.getElementById('modal');
+const btEdit = document.getElementsByClassName('edit')[0]
 
 const elementsModalDOM = {
-    header: document.getElementById('modal-header'),
+    header: document.getElementsByClassName('title')[0],
     email:document.getElementsByClassName('mEmail')[0],
     phone: document.getElementsByClassName('mPhone')[0],
     gender: document.getElementsByClassName('mGender')[0],
     company: document.getElementsByClassName('mCompany')[0],
     address: document.getElementsByClassName('mAddress')[0],
-    comments: document.getElementsByClassName('mComments')[0] 
+    comments: document.getElementsByClassName('mComments')[0], 
+    avatar: document.getElementById('mAvatar'),
+    favorite: document.getElementsByClassName('icon')[1]
 }
 
 const closeModal = function (e) {
@@ -18,27 +23,21 @@ const closeModal = function (e) {
 window.addEventListener('click', closeModal);
 
 
-export const openModal = async (contact)=>{
-
+export const openModal = (contact)=>{
     modalDiv.style.display = 'block';
-    let h = document.createElement("h3");
-
-
-    elementsModalDOM.header.setAttribute(h,);
-    elementsModalDOM.header.innerHTML;
-    innerElement(elementsModalDOM.email,contact.email);
-    innerElement(elementsModalDOM.phone,contact.info.phone);
+    elementsModalDOM.header.innerHTML = `${contact.firstName} ${contact.lastName}`;
+    elementsModalDOM.email.innerHTML = contact.email;
+    elementsModalDOM.phone.innerHTML = contact.info.phone;
+    elementsModalDOM.gender.innerHTML = contact.gender;
+    elementsModalDOM.company.innerHTML = contact.info.company;
+    elementsModalDOM.address.innerHTML = contact.info.address;
+    elementsModalDOM.comments.innerHTML = contact.info.comments;
+    elementsModalDOM.avatar.setAttribute('src',contact.info.avatar);
+    elementsModalDOM.favorite.setAttribute('src',isFavorite(contact));
 }
 
-const innerElement = (targetElement, data) =>{
-    let parent = targetElement.parentNode;
-    let newElement = document.createElement("p");
-    newElement.innerHTML=`${data}`;
+const openForm = (contact) => {
+    if(contac!=null){
 
-    if(parent.lastChild == targetElement){
-        parent.innerHTML(newElement);
-    }else{
-        parent.insertBefore(newElement, targetElement.nextSibling);
     }
-
 }
